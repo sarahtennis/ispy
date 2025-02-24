837d83adaf2815d75da7c7918ceeb441793f58bb
Sarah Tennis <sarah@Sarahs-Air.cable.rcn.com>
2025-02-24 05:03:51 -0600

    Create home page main content component and start basic styling structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
 "use client";
 import AppFooter from "@/components/app-footer/app-footer";
 import AppHeader from "@/components/app-header/app-header";
-import { 
-  NavigationService,
-  Routes,
-} from "@/services/navigation-service";
-import styles from "@/page.module.scss";
 import '@globals';
+import HomeMain from "./components/home-main/home-main";
 
 export default function HomePage() {
-  function onPlayButtonClick() {
-    NavigationService.setRedirect(false);
-    NavigationService.navigateToRoute(Routes.PLAY);
-  }
-
   return (
     <div className="body-content">
       <AppHeader></AppHeader>
-      <main>
-        <button type="button" onClick={onPlayButtonClick}>
-          Play
-        </button>
-      </main>
+      <HomeMain></HomeMain>
       <AppFooter></AppFooter>
     </div>
   );
